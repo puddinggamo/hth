a13b30febe8b88dcdee63206477750c4c6c31ba7
Candy Tang <puddinggamo@macbookpro.attlocal.net>
2025-06-18 18:21:51 -0700

    added lab2

---
+++ lab2.py
@@ -0,0 +1,21 @@
+# lab 2: Candy Tang
+
+# Step 1: Name Variable
+name = "Candy Tang"
+
+# Step 2: Nickname Variable 
+nickname = "Candy" 
+
+# Step 3 = Age Variable
+# int = Whole sum, float = decimal 
+age = 17
+
+# Step 4: Boolean Variable 
+used_before = False
+
+# Step 5: List Data structure
+hobbies = ["Sing", "draw","videos"]
+
+# Step 6: Dictionary (key:value) Pairs
+my_favs = {"drawing":"characters","Gaming":["Marvel Rivals", "Minecraft"], "Singing":"songs"} 
+print(my_favs)
