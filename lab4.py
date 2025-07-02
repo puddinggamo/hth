d6b4d3dd9f1795ea7819e77ab9082507baa8de62
Candy Tang <puddinggamo@macbookpro.attlocal.net>
2025-07-02 15:35:27 -0700

    lab4 uploaded

---
+++ lab4.py
@@ -0,0 +1,31 @@
+#control_variable = "yes"
+
+#while continue_checking == "yes":
+    #print("what is your age?")
+    #user_input = input()
+    #if user_input >= 18:
+        #print("yes you can vote")
+        #else:
+        #    print("you can't vote")
+            #print("Would you like to check another age?")
+            #user_input2 = input()
+           #checking = user_input2
+list1 = [3,-1,0,6,-4,]
+
+for x in list1: 
+
+    for x in list1:
+        if x > 0:
+            print("Value s positive")
+            elif x < == 0:
+                print("Number is zero")
+            else:
+                print("Number is zero")
+
+inventory = ["tnt", "glass", "grass", "netherite", "Waxed Lightly weathrd Chisled Copper Stairs"]
+
+for i in range(inventory):
+    if i == "Waxed Lightly weathrd Chisled Copper Stairs"
+        print("Why do you have this in your inventory?")
+    elif i == "tnt":
+        print("Go bomb Johnny house")    
