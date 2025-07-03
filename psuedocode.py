d9052b7809116d3bf91a945007045c5f2bf3482f
Candy Tang <puddinggamo@macbookpro.attlocal.net>
2025-07-02 17:18:42 -0700

    add psuedocode

---
+++ psuedocode.py
@@ -0,0 +1,20 @@
+
+
+start
+
+1. Grab the alarm click or open up the clock app on phone.
+2. Set an alarm in clock app/clock. (make sure to specify correct AM/PM time)
+3. Set a rangr of alarm to be Weston Estate "so good".
+4. Set vibrate for 30 sec on alarm.
+
+when the alarm rings:
+1. Snooze the alarm.
+    a. Give 5 more minutes.
+    b. Give 10 more minutes.
+
+    repeat till extra time is over
+    a. Give me 5 more minutes.
+    b. Stop the alarm and wake up.
+
+2. Stop the alarm and wake up.
+End
