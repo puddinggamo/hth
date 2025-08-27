d483ef84a362d574ffc47e90409cb52caf7cf089
Candy Tang <puddinggamo@gmail.com>
2025-08-27 16:30:44 -0700

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,31 +1,29 @@
-#control_variable = "yes"
+control_variable = "yes"
 
-#while continue_checking == "yes":
-    #print("what is your age?")
-    #user_input = input()
-    #if user_input >= 18:
-        #print("yes you can vote")
-        #else:
-        #    print("you can't vote")
-            #print("Would you like to check another age?")
-            #user_input2 = input()
-           #checking = user_input2
+while continue_checking == "yes":
+    print("what is your age?")
+    user_input = input()
+    if user_input >= 18:
+        print("yes you can vote")
+    else:
+        print("you can't vote")
+        print("Would you like to check another age?")
+        user_input2 = input()
+        checking = user_input2
 list1 = [3,-1,0,6,-4,]
 
-for x in list1: 
-
-    for x in list1:
+for x in list1:
         if x > 0:
             print("Value s positive")
-            elif x < == 0:
+        elif x <= 0:
                 print("Number is zero")
-            else:
+        else:
                 print("Number is zero")
 
-inventory = ["tnt", "glass", "grass", "netherite", "Waxed Lightly weathrd Chisled Copper Stairs"]
+inventory = ["tnt", "glass", "grass", "netherite", "Waxed Lightly weathered Chiseled Copper Stairs"]
 
-for i in range(inventory):
-    if i == "Waxed Lightly weathrd Chisled Copper Stairs"
+for item in range(inventory):
+    if item == "Waxed Lightly weathered Chiseled Copper Stairs":
         print("Why do you have this in your inventory?")
-    elif i == "tnt":
+    elif item == "tnt":
         print("Go bomb Johnny house")    
